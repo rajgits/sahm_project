bfe33c4082532db5bffefd32e453ed8d77a05bc3
Bharathraj <rajgits14@gmail.com>
2020-04-26 22:59:22 +0400

    new changes made

--- sahm_admin/settings.py
+++ sahm_admin/settings.py
@@ -74,27 +74,24 @@ WSGI_APPLICATION = 'sahm_admin.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME':'sahmdb',
+         'USER':'srsahmuser',
+         'PASSWORD':'srsahmuser123',
+         'HOST':'database-sr1.cuuztr0ucohu.ap-south-1.rds.amazonaws.com',
+         'PORT':'3306'
+     }
+ }
+ 
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME':'sahmpy',
-#         'USER':'root',
-#         'PASSWORD':'',
-#         'HOST':'localhost',
-#         'PORT':'3306'
-#     #     'OPTIONS': {
-#     #     'sql_mode': 'traditional',
-#     # }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 DATABASES = {
     'default': {
